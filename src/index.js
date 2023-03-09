4566b1668060f6d1e95f5ae7c965d013eed83e75
Arc-Data <arclight04@gmail.com>
2023-03-09 17:59:00 +0800

    fetch weather data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import "./main.css";
+
+const apiKey = "0227985e5e46cf9a2c09ca948590846a";
+
+const weather = (() => {
+	const getLocationWeather = async () => {
+		const response = await fetch(`
+			http://api.openweathermap.org/data/2.5/weather?q=Manila,ph&APPID=${apiKey}
+		`);
+		const weather = await response.json();
+		console.log(weather);
+		return weather;
+	};	
+
+	const getWeather = async () => {
+		const object = await getLocationWeather();
+		console.log(object);
+		const response = await fetch(`
+			http://api.openweathermap.org/data/2.5/forecast?lat=${object.coord.lat}&lon=${object.coord.lon}&appid=${apiKey}`
+		);
+		const forecast = await response.json();
+		console.log(forecast);
+		// const forecast = await response.json();
+	}
+
+	getWeather();
+})();
